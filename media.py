65154f93424c07b792ce894118e173076b011c21
Doug Wight <dcfwight@gmail.com>
2017-06-21 10:06:03 +0100

    Add video class and movie class

---
+++ media.py
@@ -0,0 +1,16 @@
+class Video():
+    """Class to represent video format media"""
+    
+    def __init__(self, title, duration):
+        self.title = title
+        self.duration = duration
+    
+class Movie(Video):
+    """Class to represent Movies - child of Video"""
+    
+    def __init__(self, title, duration, story_line, poster_image, trailer_url):
+        Video.__init__(self, title, duration)
+        self.story_line = story_line
+        self.poster_image = poster_image
+        self. trailer_url = trailer_url
+        
