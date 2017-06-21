c25f6a7c8610d6bdaee7b75ceeb5b1b15107e1dd
Doug Wight <dcfwight@gmail.com>
2017-06-21 11:40:41 +0100

    Added documentation to media.py

--- media.py
+++ media.py
@@ -1,14 +1,28 @@
 import webbrowser
 
 class Video():
-    """Class to represent video format media"""
+    """Class to represent video format media
+    Arguments:
+        title: title of the video
+        duration: duration of the video
+    """
     
     def __init__(self, title, duration):
         self.title = title
         self.duration = duration
     
 class Movie(Video):
-    """Class to represent Movies - child of Video"""
+    """Class to represent Movies - child of Video
+    
+    Arguments (inherited from Video):
+        title: title of the video
+        duration: duration of the video
+    
+    Arguments:
+        story_line: Story line of the Movie
+        poster_image_url: Link to url of poster
+        trailer_youtube_url: Link to the youtube url for the trailer
+    """
     
     def __init__(self, title, duration, story_line, poster_image_url, trailer_youtube_url):
         print ("Movie constructor called with title: "+title)
